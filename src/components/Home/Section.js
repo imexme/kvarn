b118bebdc9779f644909559f84a2ef44d44fb378
bilal-23 <mansuribilal101@gmail.com>
2021-06-21 13:07:02 +0530

    added images

---
+++ src/components/Home/Section.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+const Section = () => {
+    return (
+        <Wrap>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width:100vw;
+    height:100vh;
+    background-size:cover;
+    background-image:url('/images/model-s.jpg);
+
+`
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,14 @@
 import NavHeader from '../UI/NavHeader';
 import Menu from '../Menu/Menu';
 import HeaderBlock from './HeaderBlock';
+import Section from './Section';
+{/* <NavHeader notSticky={false} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+<Menu isMenuOpen={isMenuOpen} /> */}
 
 const Home = ({ isMenuOpen, setIsMenuOpen }) => {
     return (
         <>
-            <NavHeader notSticky={false} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-            <Menu isMenuOpen={isMenuOpen} />
-            <HeaderBlock />
+            <Section />
         </>
     )
 }
